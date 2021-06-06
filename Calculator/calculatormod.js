421e470a5822f84e2219860c580d8a7c448bc1eb
Sirak <sirak123hw@gmail.com>
2021-06-06 14:55:16 -0500

    Simple calculator

---
+++ Calculator/calculatormod.js
@@ -0,0 +1,29 @@
+var result=0;
+exports.add = function (req,res,vals) {
+    result=parseInt(vals.first)+parseInt(vals.second)
+};
+exports.subtract = function (req,res,vals) {
+    result=parseInt(vals.first)-parseInt(vals.second)
+};
+exports.multiply = function (req,res,vals) {
+    result=parseInt(vals.first)*parseInt(vals.second)
+};
+exports.division = function (req,res,vals) {
+    result=(parseInt(vals.first))/(parseInt(vals.second))
+};
+exports.displays=function(req,res,vals){
+    res.writeHead(200, {'Content-Type': 'text/html'}); 
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
+    res.write("<head><meta charset=\"utf-8\"/>"); 
+    res.write("<title>Calculator Web Site</title>"); 
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<p>The result is: ");
+          res.write(String(result));
+    res.write("</p>");
+    res.write("<p><a href = 'calculator.html'>Another Calculation</a></p>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+};
